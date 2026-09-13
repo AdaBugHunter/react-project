e368f3ed730658a7f131611e3e27925bbe38feab
AdaBugHunter <aliareeba261@gmail.com>
2026-09-13 06:12:20 +0500

    Refactor StockStatus into components directory

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import MessageToggle from './components/MessageToggle.jsx';
 import NameInput from './components/NameInput.jsx';
 import StudentForm from './components/StudentForm.jsx';
 import LoginStatus from './components/LoginStatus.jsx';
-import StockStatus from './StockStatus.jsx';
+import StockStatus from './components/StockStatus.jsx';
 
 function App() {
   const handleNameChange = (name) => {
